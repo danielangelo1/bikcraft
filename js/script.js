728053de606acf8149dd1b12cd01a1396ac0f98b
Daniel <danielangelo1234@gmail.com>
2023-03-20 11:00:40 -0300

    added toggle questions

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const parametros = new URLSearchParams(location.search)
 
 function ativarProduto(parametro){
     const elemento = document.getElementById(parametro);
-    
+
     if(elemento){
         elemento.checked=true;
         console.log(elemento);
@@ -26,3 +26,25 @@ function ativarProduto(parametro){
 }
 
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains('ativa');
+    pergunta.setAttribute('aria-expanded', 'true');
+
+    console.log(resposta)
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
