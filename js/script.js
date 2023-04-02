2dc5fee366cace506a4a6c79edddd4909cc18691
Daniel <danielangelo1234@gmail.com>
2023-04-02 09:33:31 -0300

    added switchable imgs and fixed bugs

--- js/script.js
+++ js/script.js
@@ -1,50 +1,68 @@
-
 // Ativar Links do Menu
-const links = document.querySelectorAll('.header-menu a');
+const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(link){
-    const url = location.href;
-    const href = link.href;
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
 
-    if(url.includes(href)){
-        link.classList.add('ativo');
-    }
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
 
 links.forEach(ativarLink);
 
-// Ativar Itens do Orçamento 
-const parametros = new URLSearchParams(location.search)
+// Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
 
-function ativarProduto(parametro){
-    const elemento = document.getElementById(parametro);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
 
-    if(elemento){
-        elemento.checked=true;
-        console.log(elemento);
-    }
+  if (elemento) {
+    elemento.checked = true;
+    console.log(elemento);
+  }
 }
 
 parametros.forEach(ativarProduto);
 
 // Perguntas Frequentes
 
-const perguntas = document.querySelectorAll('.perguntas button');
+const perguntas = document.querySelectorAll(".perguntas button");
 
-function ativarPergunta(event){
-    const pergunta = event.currentTarget;
-    const controls = pergunta.getAttribute('aria-controls');
-    const resposta = document.getElementById(controls);
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
 
-    resposta.classList.toggle('ativa');
-    const ativa = resposta.classList.contains('ativa');
-    pergunta.setAttribute('aria-expanded', 'true');
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", "true");
 
-    console.log(resposta)
+  console.log(resposta);
 }
 
-function eventosPerguntas(pergunta){
-    pergunta.addEventListener('click', ativarPergunta);
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
